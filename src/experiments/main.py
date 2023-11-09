06eb2b44a14f09678d1230dd3b81490962a2d919
kdf_7 <francesco.defaveri1999@gmail.com>
2023-11-09 23:27:44 +0100

    first attempt of sampling laplacian and gamma

--- src/experiments/main.py
+++ src/experiments/main.py
@@ -7,10 +7,7 @@ import prepare_data as prep
 import random
 import tqdm
 import nltk
-nltk.download('averaged_perceptron_tagger')
-sns.set_style("whitegrid")
-sns.set_context("paper")
-sns.set(font_scale=1.5)
+#nltk.download('averaged_perceptron_tagger')
 
 def main():
     model = glove.model(dimension=50)
@@ -21,30 +18,50 @@ def main():
 
     vocab_embeddings_queries = prep.get_vocab_embeddings(data, model)
 
-    query = 'what slows down the flow of blood'        
+    query = 'how many people use google in one day' #melanoma skin cancer symptoms #what causes ankle blisters  
+                                                #what slows down the flow of blood
     
     print('-'*50)
     tokenized_query = query.split()
     tokenized_query = nltk.pos_tag(tokenized_query)
     print('Tokenized query: {}'.format(tokenized_query))
-    
-    k=10
+    #define mechanism parameters
+    k=100
+    scale_param = 2
+    shape = 1
     new_query_distance_based = []
     new_query_angle_based = []
     for word, tag in tokenized_query:
-        if tag == 'NN':
-            #obfuscate
-            rank_distance, secure_box_distance = obfuscator.get_safe_box(word, vocab_embeddings_queries, model, k, 'distance')
-            rank_angle, secure_box_angle = obfuscator.get_safe_box(word, vocab_embeddings_queries, model, k, 'angle')
-            #remove secure box from rank
-            rank_distance = [x for x in rank_distance if x not in secure_box_distance]
-            rank_angle = [x for x in rank_angle if x not in secure_box_angle]
+        #if tag == 'NN' or tag == 'NNS' or tag == 'JJ':
+            #get boxes
+            rank_distance, candidates_distance, safe_box_distance = obfuscator.get_safe_box(word, vocab_embeddings_queries, model, k, 'distance')
+            rank_angle, candidates_angle, safe_box_distance = obfuscator.get_safe_box(word, vocab_embeddings_queries, model, k, 'angle')
+        #else:
+            candidates_distance = [(word, 0)]
+            candidates_angle = [(word, 0)]
+
+        #print('-'*50)
+        #print('Word: {}'.format(word))
+        #print('Candidates distance based: {}'.format(candidates_distance))
+        #print('Candidates angle based: {}'.format(candidates_angle))
+        #print('-'*50)
+        
+        #get weights
+        #laplace
+        #weights = obfuscator.get_weights('laplace', len(candidates_distance), scale_param) 
+        #normal
+        #weights = obfuscator.get_weights('normal', len(candidates_distance), scale_param) 
+        #gamma
+        #weights = obfuscator.get_weights('gamma', len(candidates_distance), scale_param, shape) 
+        #uniform
+            weights = obfuscator.get_weights('uniform', len(candidates_distance), scale_param) 
+
             #get new query
-        else:
-            secure_box_distance = [(word, 0)]
-            secure_box_angle = [(word, 0)]
-        new_query_distance_based.append(random.choice(secure_box_distance)[0])
-        new_query_angle_based.append(random.choice(secure_box_angle)[0])
+            new_word_distance_based = obfuscator.get_new_word(candidates_distance, weights)
+            new_word_angle_based = obfuscator.get_new_word(candidates_angle, weights)
+
+            new_query_distance_based.append(new_word_distance_based)
+            new_query_angle_based.append(new_word_angle_based)
 
     print('Parameter used: k = {}'.format(k))
     print('-'*50)
--- src/experiments/obfuscator.py
+++ src/experiments/obfuscator.py
@@ -1,16 +1,41 @@
 import numpy as np
-import pandas as pd
 import distance
-import math
+import random
 
 def get_safe_box(word, word_embeddings_queries, model, k=3, feature='distance'):
     if feature == 'distance':
         rank = distance.get_rank(word, word_embeddings_queries, model, feature)
-        secure_box = rank[:k]
-        return rank, secure_box
+        candidates = rank[k:]
+        secure_words = rank[:k]
+        return rank, candidates, secure_words
     elif feature == 'angle':
         rank = distance.get_rank(word, word_embeddings_queries, model, feature)
-        secure_box = rank[:k]
-        return rank, secure_box
+        candidates = rank[k:]
+        secure_words = rank[:k]
+        return rank, candidates, secure_words
     else:
         raise ValueError('Feature not supported')
+    
+def get_weights(distribution, size, scale, shape = 2):
+    if distribution == 'laplace':
+        weights = np.random.laplace(loc=0, scale=scale, size=size)
+        weights = np.abs(weights)
+        weights = weights/np.sum(weights)
+        return sorted(weights)
+    elif distribution == 'uniform':
+        weights = np.random.uniform(low=0.0, high=1.0, size=size)
+        return weights
+    elif distribution == 'normal':
+        weights = np.random.normal(loc=0.0, scale=scale, size=size)
+        weights = weights/np.sum(weights)
+        return sorted(weights)
+    elif distribution == 'gamma':
+        weights = np.random.gamma(shape, scale, size)
+        weights = weights/np.sum(weights)
+        return sorted(weights)
+    else:
+        raise ValueError('Distribution not supported')
+
+def get_new_word(secure_box, weights):
+    new_word = random.choices(secure_box, weights)[0][0]
+    return new_word
