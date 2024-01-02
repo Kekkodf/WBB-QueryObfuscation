b536b19d27d976efbf5b3ba702a7501b0b57f5bb
kdf_7 <109605653+Kekkodf@users.noreply.github.com>
2024-01-02 16:51:58 +0100

    Update preprocess.py

--- src/pipeline/preprocess.py
+++ src/pipeline/preprocess.py
@@ -1,17 +1,20 @@
 import gensim.downloader as api
 import nltk
+import numpy as np
+from tqdm import tqdm
 #nltk.download('punkt')
 #nltk.download('averaged_perceptron_tagger')
+path = './data/embeddings/'
 
 def model(dimension):
     if dimension == 50:
-        return api.load('glove-wiki-gigaword-50')
+        return api.load('glove-wiki-gigaword-50'), api.load('glove-wiki-gigaword-50').index_to_key
     elif dimension == 100:
-        return api.load('glove-wiki-gigaword-100')
+        return api.load('glove-wiki-gigaword-100'), api.load('glove-wiki-gigaword-100').index_to_key
     elif dimension == 200:
-        return api.load('glove-wiki-gigaword-200')
+        return api.load('glove-wiki-gigaword-200'), api.load('glove-wiki-gigaword-200').index_to_key
     elif dimension == 300:
-        return api.load('glove-wiki-gigaword-300')
+        return api.load('glove-wiki-gigaword-300'), api.load('glove-wiki-gigaword-300').index_to_key
     else:
         raise ValueError('Invalid dimension: {}'.format(dimension))
     
@@ -33,8 +36,6 @@ def get_vocab(model):
     #print('Vocab size: {}'.format(len(vocab_filtered)))
     #print('Filtered vocab size: {}'.format(len(filtered)))
     #print('Intersection: {}'.format(len(vocab_filtered.intersection(set(filtered)))))
-    #remove all words that contain numbers
-    vocab = [word for word in vocab if not any(char.isdigit() for char in word)]
     return vocab#, embeddings
 
 '''
@@ -51,3 +52,30 @@ def tokenize_query(query):
 #
 def get_POS_tags(query):
     return nltk.pos_tag(query)
+
+def initialize_glove_optimized(optimization = False):
+    if optimization == True:
+        with open(path + 'glove.6B.300d.txt', 'r', encoding='utf-8') as f:
+            glove_normal = f.readlines()
+        with open(path + 'glove_counterFitt.txt', 'r', encoding='utf-8') as f:
+            glove_optimal = f.readlines()
+        glove_normal = [line.strip().split(' ') for line in tqdm(glove_normal)]
+        glove_optimal = [line.strip().split(' ') for line in tqdm(glove_optimal)]
+        vocab_normal = [word[0] for word in glove_normal]
+        vocab_optimal = [word[0] for word in glove_optimal]
+        glove_normal = {word[0]: np.array(word[1:], dtype=np.float32) for word in tqdm(glove_normal)}
+        glove_optimal = {word[0]: np.array(word[1:], dtype=np.float32) for word in tqdm(glove_optimal)}
+        #substitute the optimal embeddings to the normal ones
+        for word in tqdm(vocab_optimal):
+            if word in vocab_normal:
+                glove_normal[word] = glove_optimal[word]
+        return glove_normal, vocab_normal
+    elif optimization == False:
+        with open(path + 'glove.6B.300d.txt', 'r', encoding='utf-8') as f:
+            glove = f.readlines()
+        glove = [line.strip().split(' ') for line in tqdm(glove)]
+        vocab = [word[0] for word in glove]
+        glove = {word[0]: np.array(word[1:], dtype=np.float32) for word in tqdm(glove)}
+        return glove, vocab
+    else:
+        raise ValueError('optimization must be True or False')
