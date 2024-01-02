d173fe5f054181978b8486593d0304300f65e59e
kdf_7 <109605653+Kekkodf@users.noreply.github.com>
2024-01-02 16:50:20 +0100

    Update obfuscation.py
    Fixed bugs

--- src/pipeline/obfuscation.py
+++ src/pipeline/obfuscation.py
@@ -4,6 +4,7 @@ import random
 import pandas as pd
 from tqdm import tqdm
 tqdm.pandas()
+import time
 from functools import partial
 '''
 Obfusctaion phases:
@@ -20,23 +21,46 @@ Parameters:
     - distribution:= Distribution for candidates selection
 '''
 
-desired_pos_tags = ['NN', 'NNS', 'NNP', 'NNPS', 'JJ']
+desired_pos_tags = ['NN', 'NNS', 'NNP', 'NNPS', 'JJ', 'JJS']
 
-def obfuscate(row, model, k, n, distribution, feature):
+def obfuscate(row, model, k, n, epsilon, feature):
     '''
     The method obfuscates a query using the distance based obfuscation technique.
     '''
-    new_query = row.progress_apply(compute_query, args=(model, k, n, distribution, feature))
+    new_query = row.progress_apply(compute_query, args=(model, k, n, epsilon, feature))
     return new_query
     
-def compute_query(row, model, k, n, distribution, feature):
-    query_obfuscated = [word[0] for word in row]
-    try:
-        x_words_embs = np.array([model.get_word_embedding(word[0]) for word in row if word[1] in desired_pos_tags])
-        partial_get_candidates = partial(model.get_k_closest_terms, k=k, n=n, feature=feature)
-        candidates, safe_box = partial_get_candidates(x_words_embs)
-        extracted = model.candidate_extraction(candidates, distribution)
-        query_obfuscated = [word[0] if word[1] not in desired_pos_tags else extracted.pop(0) for word in row]
-    except:
-        pass
-    return ' '.join(query_obfuscated)
+def compute_query(row, model, k, n, epsilon, feature):
+        try:
+            x_words_embs = np.array([model.get_word_embedding(word[0]) for word in row if word[1] in desired_pos_tags])
+            candidates, scores, safe_box = model.get_k_closest_terms(x_words_embs, k, n, epsilon, feature)
+            #candidates and scores must be of the same dimensions
+
+            # List to store the selected candidates
+            selected_candidates = []
+            # Iterate over each row
+            for i in range(candidates.shape[0]):
+                # Select one candidate from the current row
+                selected = random.choices(candidates[i], weights=scores[i], k=1)[0]
+                # Append the selected candidate to the list
+                selected_candidates.append(selected)
+
+            #print(selected_candidates)
+            
+            #print('Extracted: ', extracted)
+            query_obfuscated = [word[0] if word[1] not in desired_pos_tags else selected_candidates.pop(0) for word in row]
+            #print('Query obfuscated: ', query_obfuscated)
+            #time.sleep(30)
+            res = ' '.join(query_obfuscated)
+            #print(res)
+            return res
+        except KeyError:
+            res = ' '.join([word[0] for word in row])
+            #print('Key Error: ', res)
+            return res
+        except IndexError:
+            res = ' '.join([word[0] for word in row])
+            #print('Index Error: ', res)
+            return res
+        
+    
