6d1639d13badaeb6d512f26bcf4f458ae4ea14e6
kdf_7 <109605653+Kekkodf@users.noreply.github.com>
2024-01-02 16:51:19 +0100

    Update WordEmbeddings.py
    new mechanism and bug fixed

--- src/pipeline/WordEmbeddings.py
+++ src/pipeline/WordEmbeddings.py
@@ -1,10 +1,18 @@
 import numpy as np
 import random
+import nltk
+import time
+#nltk.download('words')
+#nltk.download('stopwords')
+
+words = set(nltk.corpus.words.words())
+stopwords = nltk.corpus.stopwords.words('english')
+
 
 class WordEmbeddings:
     def __init__(self, model, vocab):
         # Filter out words with any non-alphabetic characters from the vocabulary
-        self.vocab = [word for word in vocab if word.isalpha()]
+        self.vocab = [word for word in vocab if word not in stopwords and word.isalpha() ]
         self.model = model
         #size of the embeddings
         self._embeddings = {word: model[word] for word in self.vocab}
@@ -21,33 +29,53 @@ class WordEmbeddings:
     def get_word_embedding(self, word):
         return self._embeddings[word]
     
-    def get_k_closest_terms(self, vectors, k, n, feature):
-        if feature == 'distance':
-            distance = euclidean_distance_matrix(vectors, self._embeddings_matrix)
-            found_candidates = distance.argsort(axis=1)[:, k:k*n]
-            found_safe = distance.argsort(axis=1)[:, :k]
-        elif feature == 'angle':
-            distance = cosine_distance_matrix(vectors, self._embeddings_matrix)
-            found_candidates = distance.argsort(axis=1)[:, -k*n:-k]
-            found_safe = distance.argsort(axis=1)[:, -k:]
-        elif feature == 'product':
-            distance = product_metrix(vectors, self._embeddings_matrix)
-            found_candidates = distance.argsort(axis=1)[:, k:k*n]
-            found_safe = distance.argsort(axis=1)[:, :k]
+    def get_k_closest_terms(self, vectors, k, n, epsilon, feature):
+        #print('Vector shape: ', vectors.shape)
+        if n > k:
+            if feature == 'distance':
+                distance = euclidean_distance_matrix(vectors, self._embeddings_matrix)
+                found_candidates = distance.argsort(axis=1)[:, k:k+n] #n candidates sorted by distance
+                found_safe = distance.argsort(axis=1)[:, :k] #k safe sorted by distance
+                #get values of distances candidates
+                candidates_distances = np.take_along_axis(distance, found_candidates, axis=1)
+            elif feature == 'angle':
+                distance = cosine_distance_matrix(vectors, self._embeddings_matrix)
+                found_candidates = distance.argsort(axis=1)[:, k:k+n] #n candidates sorted by distance
+                found_safe = distance.argsort(axis=1)[:, :k] #k safe sorted by distance
+                #get values of distances candidates
+                candidates_distances = np.take_along_axis(distance, found_candidates, axis=1)
+            elif feature == 'product':
+                distance = product_metrix(vectors, self._embeddings_matrix)
+                found_candidates = distance.argsort(axis=1)[:, k:k+n] #n candidates sorted by distance
+                found_safe = distance.argsort(axis=1)[:, :k] #k safe sorted by distance
+                #get values of distances candidates
+                candidates_distances = np.take_along_axis(distance, found_candidates, axis=1)
+            else:
+                raise ValueError('Feature not supported!')
         else:
-            raise ValueError('Feature not supported!')
-
+            raise ValueError('n must be greater than k!')
+        #compute candidates scores based on distances, the lower the distance the higher the score
+        mean = candidates_distances.mean()
+        std = candidates_distances.std()
+        candidates_scores = [(1/(1+np.exp((distance - mean)/(std))))*epsilon/2 for distance in candidates_distances]   
+        candidates_scores = candidates_scores/np.sum(candidates_scores)
+        
+        # Convert the indexes to words
         candidates = self.indexes_to_words(found_candidates)
+        #convert candidates in a matrix len(vectors) x n
+        candidates = np.array(candidates).reshape(len(vectors), n)
+
+        #create tuples (word, distance)
         safe = self.indexes_to_words(found_safe)
-        return candidates, safe
+        
+        return candidates, candidates_scores, safe
         
     def indexes_to_words(self, indexes):
         return [self._int2word[e] for f in indexes for e in f]
 
-    def candidate_extraction(self, candidates_box, distribution):
-        probabilities = get_probabilities_of_extraction(candidates_box, distribution)
-        new_word = random.choices(candidates_box, probabilities)[0]
-        return new_word
+    def candidate_extraction(self, candidates_boxes, distribution):
+        probabilities = get_probabilities_of_extraction(candidates_boxes, distribution)
+        return list(random.choices(candidates_boxes, weights=probabilities, k=len(candidates_boxes)))
 
 @staticmethod
 def euclidean_distance_matrix(x, y):
@@ -85,7 +113,7 @@ def get_probabilities_of_extraction(candidates_box, distribution):
         probabilities = sorted(probabilities, reverse=True)
         return probabilities
     elif distribution[0] == 'gamma':
-        probabilities = np.random.gamma(distribution[1][0], distribution[1][1], size = len(candidates_box))
+        probabilities = np.random.exponential(scale=distribution[1][1], size=len(candidates_box))
         probabilities = probabilities/np.sum(probabilities)
         #sort probabilities
         probabilities = sorted(probabilities, reverse=True)
